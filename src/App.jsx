c0f8b7a310cf830d0a21d2ad21f30b7ca3372933
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-04-11 18:48:01 +0530

    Updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,57 @@
 import React, { useState, useEffect } from 'react'
 
+// packages
 import { nanoid } from 'nanoid';
 import styled from 'styled-components';
 
-import Quiz from "./Quiz"
+// Components
+import Quiz from "./components/Quiz"
 
-// COMPONENTS
-
-// STYLES
+// GlobalStyle
 import { GlobalStyle } from './GlobalStyle';
 
-
-// 
-//
-// https://opentdb.com/api.php?amount=5&encode=base64
-// https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple&encode=base64
-//  const uri = `https://opentdb.com/api.php?amount=5&category=${category}&difficulty={level}&type=multiple&encode=base64`
-
-// categories 
-//  general knowledge  = 9  
-//  books = 10 
-//  films =  11
-//  music  = 12  
-//  musicals and theatre = 13
-//  television = 14
-//  video games = 15
-//  board games = 16
-//  science and nature = 17
-//  science computers = 18
-//  mathematic = 19
-//  mytholog = 20
-//  sports = 21
-//  geography =22 
-//  history =  23
-//  politics = 24 
-//  art = 25 
-//  celebrities = 26
-//  animals = 27
-//  vehicles = 28
-//  comics = 29
-//  science gadgets = 30
-//  japanese anime and manga = 31
-//  cartoons and animations = 32 
-
-//difficulty
-//easy  medium hard
-
-
-// quiz question fetch f()
-// choose a category, assign the number of that category to a variable
-// option for random category assign that the variable
-// choose a difficulty, assign the difficulty to a variable
-// build a URI for fetch
-// use the URI to fetch 5 questions, pass each question through the decoder f() 
-// https://developer.mozilla.org/en-US/docs/Glossary/Base64 
-// a f() to sort answers (random sort or just sort())
-// save these to a questionData state
-
-// componet start page
-// title,  categories, difficulty and a start button
-// start button should pass the URI to quiz fetch f() 
-// when questionData state is updated, load the quizpage component
-
-//quiz page component
-// map questionData to display 5 questions(quizQuestion component) in DOM
-  // quizQuestion component 
-  // load question & answer data
-  // save and highlight selection in green
-  // know the correct answer
-// on submit compare selection to correct answer
-// display correct answer as green and selected incorrect answer as red
-// tally correct answer and show score
-// check answer submit button updates to play again to reset the game
-// reset f() - reset the state, reset the score, reset the fetch URI show start page again
-
-
-
 const Wrapper = styled.section`
-padding: 4em;
-background: var(--clr - body - bg);
-height: 100vh;
+  padding: 4em 8em;
+  max-width: 1440px;
+  background: var(--clr - body - bg);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  place-items: center;
+  gap: 1.5em;
 `;
 
 const Title = styled.h1`
-font - size: 1.5em;
-text - align: center;
+  font-size: 1.5em;
+  text-align: center;
 `;
+
+const Content = styled.p`
+  max-width: 50ch;
+  text-align: center;
+  font-family: var(--ff-secondary);
+  font-weight: var(--fw-reg);
+`
+
+const TestButton = styled.button `
+
+    &:hover,
+    &:focus {
+      background-color: var(--clr-highlight-secondary);
+      color: var(--clr-button-bg);
+    }
+`
 
 function App() {
 
-  // THE DECODED QUESTION DATA
+  // the decoded quiz data
   const [quizData, setQuizData] = useState('');
   
-  // START TO BEING FETCHING DATA FROM THE API
-  const [quizStart, setQuizStart] = useState(true);
+  // toStart fetching data
+  const [quizStart, setQuizStart] = useState(false);
   
-  // TODO : LOADER TO SHOW WHILE DATA IS BEING FETCHED
+  // TODO : A loader while data is being fetched.
 
 
   function dataSplit(encodedData) {
@@ -148,16 +104,16 @@ function App() {
 
   return (
     <>
+      <GlobalStyle/>
     <Wrapper>
       {!quizStart  ? 
         ( <>
           <Title>Quizzical App</Title>
-          <p>In each quiz you'll be given five random quesitons to answer. Press the button below to play and see how many you can answer correctly! </p>
-          <button onClick = {loadQuiz}>Start Quiz</button> </> )
+          <Content>In each quiz you'll be given five random quesitons to answer. Press the button below to play and see how many you can answer correctly! </Content>
+          <TestButton onClick = {loadQuiz}>Start Quiz</TestButton> </> )
         :  <Quiz quizData = {quizData}/> }
            {/* loader when fetching data */}
     </Wrapper>
-    <GlobalStyle/>
     </>
   )
 }
