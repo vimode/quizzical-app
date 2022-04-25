86cee2004e7939dcc0b3db2cf3810f5739bcc071
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-04-25 20:05:46 +0530

    Added category and difficulty option

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 
 // packages
 import { nanoid } from 'nanoid';
@@ -20,7 +20,80 @@ function App() {
   // A loading icon while data is being fetched.
   const [isLoading, setIsLoading] = useState(false)
 
+  // categories
+  const [quizCategories, setQuizCategories] = useState([]);
 
+  // intial URL parameters
+  const fetchURLParameters = {
+    selectedCategory: "",
+    selectedDifficulty: "",
+    fetchURL : `https://opentdb.com/api.php?amount=5&encode=base64`
+  };
+
+  //udpating URL parameters
+  const [state, dispatch] = useReducer(reducer, fetchURLParameters )
+
+  // fetch quizCategories
+  useEffect(()=> {
+    const fetchCategories = async () => {
+      const categoriesData = await fetch('https://opentdb.com/api_category.php');
+      const jsonCategoriesData = await categoriesData.json()
+      setQuizCategories(jsonCategoriesData.trivia_categories)
+    }
+
+    fetchCategories()
+    .catch(console.error);
+  }, [])
+
+
+  //handle URL paramter changes
+  const handleParameterUpdate = (event) => {  
+  const { value, name } = event.target;
+    if (name === "category") {
+      return ( dispatch ( {type : 'category', payload : value}));
+    }
+    dispatch ( {type: 'difficulty', payload : value});
+  }
+
+// reducer function
+  function reducer(state, action) {
+    switch(action.type) {
+      case 'category' : 
+        return {...state,
+        selectedCategory : action.payload,
+        fetchURL : `https://opentdb.com/api.php?amount=5&category=${action.payload}&difficulty=${state.selectedDifficulty}&type=multiple&encode=base64`};
+      case 'difficulty' :
+        return {...state,
+        selectedDifficulty :action.payload,
+        fetchURL : `https://opentdb.com/api.php?amount=5&category=${state.selectedCategory}&difficulty=${action.payload}&type=multiple&encode=base64`};
+      default:
+        throw new Error();
+    }
+  }
+  
+  // fetch base64 encoded data from API
+  async function fetchQuizData () {
+    
+    setIsLoading(true)
+
+    let fetchURI = ""
+    if(fetchURLParameters.selectedCategory && fetchURLParameters.selectedDifficulty) {
+      fetchURI = fetchURLParameters.fetchURL
+    } else {
+      fetchURI = 'https://opentdb.com/api.php?amount=5&encode=base64'
+    }
+
+    const response = await fetch(fetchURI);
+    try {
+      const fetchedData = await response.json()
+      console.log(fetchedData.results)
+      dataSplit(fetchedData.results)
+    } catch(e) {
+      console.log(`Error ${response.status}`)
+    }
+  }
+
+  // split base64 encoded data to convert to utf-8 and store in a state variable.
   function dataSplit(encodedData) {
     
     let decodedData = []
@@ -33,6 +106,8 @@ function App() {
         id : nanoid(),
         question : atob(questionObj.question),
         correct_answer : atob(questionObj.correct_answer),
+        category: atob(questionObj.category),
+        difficulty: atob(questionObj.difficulty),
         answer_options : combined_options.sort().map((option) => 
           atob(option)
         )
@@ -42,32 +117,13 @@ function App() {
     setQuizData(decodedData)
     setIsLoading(false)
   }
-  
 
-  async function fetchQuizData () {
-    
-    setIsLoading(true)
-
-    const fetchURI = "https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple&encode=base64"
-    
-    const response = await fetch(fetchURI);
-    try {
-      const quizData = await response.json()
-      dataSplit(quizData.results)
-    } catch(e) {
-      console.log(`Error ${response.status}`)
-    }
-  }
-
+  // start quiz
   function loadQuiz () {
     setQuizStart(true)
   }
 
-  function resetStart() {
-    setQuizStart(false)
-    setQuizData('')
-  }
-
+    // triggers when user clicks to start quiz
   useEffect(()=> {
     if(quizStart) {
       fetchQuizData()
@@ -75,19 +131,53 @@ function App() {
   }, [quizStart])
 
 
+  // reset quiz
+  function resetStart() {
+    setQuizStart(false)
+    setQuizData('')
+  }
+
+
   return (
-    <>
     <Wrapper>
       
       {!isLoading && !quizStart  ? 
         ( <>
           <Title>Quizzical App</Title>
+
+         {quizCategories.length > 1 ? 
+         <>
+          <label htmlFor='category_select'>Choose a Category: 
+          <select name="category" id="category_select" onChange={(event)=> handleParameterUpdate(event)}>
+            <option value ="">Select a Category</option>
+            {quizCategories.map((categoryName) => 
+             <option 
+              key = {categoryName.id} 
+              value={categoryName.id}>
+              {categoryName.name}
+            </option>
+          )} 
+          </select>
+          </label>
+
+          <label htmlFor = 'difficulty_select'
+          onChange={(event)=> handleParameterUpdate(event)}>
+            Choose a difficulty: 
+          <select name = "difficulty" id="difficulty_select">
+            <option value="">Select Quiz Difficulty</option>
+            <option value ="easy">Easy</option>
+            <option value ="medium">Medium</option>
+            <option value ="hard">Hard</option>
+          </select>
+          </label>
+
+         </> : <p>Loading Options...</p>}
+
           <Content>In each quiz you'll be given five random quesitons to answer. Press the button below to play and see how many you can answer correctly! </Content>
           <StartButton onClick = {loadQuiz}>Start Quiz</StartButton> </> ) 
           :  isLoading && quizData.length < 4 ? (<Loader/>) 
         : !isLoading && quizData ? (<Quiz quizData = {quizData} resetStart= {resetStart}/>) : <></> }
     </Wrapper>
-    </>
   )
 }
 
